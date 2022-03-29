be0dda5eca556cc899d9204a0c5fb16887cf82c9
gnaneswar <gnaneswar.9@gmail.com>
2022-03-29 20:05:19 +0530

    ContentVC added

---
+++ GitExample/ContentViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  GitExample
+//
+//  Created by lakshmi Raghavendra on 29/03/22.
+//
+
+import UIKit
+
+class ContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    //No
+    
+
+    
+}
