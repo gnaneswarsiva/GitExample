0dc701c9361a1a09cbf0f536bc7b604d0f828518
gnaneswar <gnaneswar.9@gmail.com>
2022-03-29 20:07:47 +0530

    renamed contentVC to MainVC

--- GitExample/MainViewController.swift
+++ GitExample/MainViewController.swift
@@ -7,14 +7,14 @@
 
 import UIKit
 
-class ContentViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    //No
+    //Nothing happened
     
 
     
